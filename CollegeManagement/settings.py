994d7e056f532231a8292b164e0a845d84639765
jubaiyerahammed <jubaiyerahammed2886@gmail.com>
2026-05-05 13:47:51 +0900

    StudyHouse

--- CollegeManagement/settings.py
+++ CollegeManagement/settings.py
@@ -17,6 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 #print(BASE_DIR)
 TEMPLATE_DIR=BASE_DIR/'temlates'
 #print(TEMPLATE_DIR)
+STATIC_DIR = BASE_DIR/'static'
 
 
 # Quick-start development settings - unsuitable for production
@@ -124,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIR = [STATIC_DIR,]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
