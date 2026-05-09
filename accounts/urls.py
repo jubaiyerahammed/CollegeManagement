eb1c4b469f096e72275a79c4d27bc1672535d0b7
jubaiyerahammed <jubaiyerahammed2886@gmail.com>
2026-05-10 03:04:34 +0900

    accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 
 urlpatterns = [
-    path('li/', views.login_view),
-    path('lo/', views.logout_view),
-    path('rs/', views.register_student),
-    path('rt/', views.register_teacher),
+    path('li/', views.login_view, name='login'),
+    path('lo/', views.logout_view, name='logout'),
+    path('rs/', views.register_student, name='register_student'),
+    path('rt/', views.register_teacher, name='register_teacher'),
     
 ]    
