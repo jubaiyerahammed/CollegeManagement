1353730a8691d871bb187aeda1b281c877278153
jubaiyerahammed <jubaiyerahammed2886@gmail.com>
2026-05-17 13:31:28 +0900

    password

--- students/forms.py
+++ students/forms.py
@@ -6,4 +6,13 @@ class StudentRegistration(forms.Form):
     email= forms.EmailField()
     batch=forms.IntegerField()
     password= forms.CharField(widget=forms.PasswordInput())
+    re_password= forms.CharField(widget=forms.PasswordInput())
+    
     textarea=forms.CharField(widget=forms.Textarea())
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password_match = self.cleaned_data['password']
+        re_password_match = self.cleaned_data['re_password']
+        if password_match != re_password_match :
+            raise forms.ValidationError("password doesn't match")
